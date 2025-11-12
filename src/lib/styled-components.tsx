7514141aca0a6ef57b6aa8d01e83f5e2e7953b43
Marko Siilak <marko@siilak.com>
2025-11-12 13:56:17 +0200

    chore: set up styled-components infrastructure

---
+++ src/lib/styled-components.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useServerInsertedHTML } from "next/navigation";
+import { PropsWithChildren, useState } from "react";
+import { ServerStyleSheet, StyleSheetManager } from "styled-components";
+
+export default function StyledComponentsRegistry({ children }: PropsWithChildren) {
+  const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet());
+
+  useServerInsertedHTML(() => {
+    const styles = styledComponentsStyleSheet.getStyleElement();
+    styledComponentsStyleSheet.instance.clearTag();
+    return <>{styles}</>;
+  });
+
+  if (typeof window !== "undefined") {
+    return <>{children}</>;
+  }
+
+  return (
+    <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+      {children as React.ReactNode}
+    </StyleSheetManager>
+  );
+}
+
